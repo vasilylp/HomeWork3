ef38e38b986fcf606c8d028d83b55e493bcf0dc4
Vpolyushkevich@gmail.com <Vasily>
2023-01-24 19:59:14 +0300

    Add in Task3 new versin

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -26,4 +26,23 @@ else
     System.Console.WriteLine($"{text} не пятизначное число. Введите пятизначное число");
 }
 
+System.Console.WriteLine("Введите пятизначное число :");
+string? text = Console.ReadLine();
+
+
+// if (text!.Length == 5)
+// {
+//     if (text[0] == text[text.Length - 1] && text[1] == text[text.Length - 2]) // определение последних элементов массива
+//     {
+//         System.Console.WriteLine($"Число {text} палиндром - Да");
+//     }
+//     else
+//     {
+//         System.Console.WriteLine($"Число {text} не палиндром - Нет");
+//     }
+// }
+// else
+// {
+//     System.Console.WriteLine($"{text} не пятизначное число. Введите пятизначное число");
+// }
 
